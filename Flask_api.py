efb3d9162ca3519f42793f16a739c8dc9f1dd93b
manjikkk47 <147363429+manjikkk47@users.noreply.github.com>
2025-09-28 12:37:37 +0530

    Add files via upload

---
+++ Flask_api.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 28 11:51:17 2025
+
+@author: manji
+"""
+from flask import Flask, request
+import pandas as pd
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+pickle_in  = open('C:/Users/manji/OneDrive/Desktop/ML model as API/Python Code/ML Stuffs/classifier.pkl','rb')
+classifier = pickle.load(pickle_in)
+
+@app.route('/')
+def welcome():
+    return "Welcome All"
+
+
+@app.route('/predict')
+def predict_note_authentication():
+    variance = request.args.get('variance')
+    skewness = request.args.get('skewness')
+    curtosis = request.args.get('cutrosis')
+    entropy = request.args.get('entropy')
+    prediction = classifier.predict([[variance,skewness,curtosis,entropy]])
+    return " The predicted values is "+str(prediction)
+
+@app.route('/predict_file', methods=["POST"])
+def predict_note_file():
+    df_test=pd.read_csv(request.files.get("file"))
+    prediction=classifier.predict(df_test)
+    return "The predicted values for the csv is"+str(list(prediction))
+
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run()
